5799687350b2e8ca0887d0ffee54a214e06d0672
Rohit kumar barada <rohitbarada@gmail.com>
2024-04-16 02:22:33 +0530

    till 17

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,useNavigate } from "react-router-dom";
 import { SignUp } from "./routes/signup";
 import { SignIn } from "./routes/signin";
-import { Dashboard } from "./routes/dashbord";
+import Dashboard from "./routes/dashbord";
 import { Send } from "./routes/send";
 function App() {
-
+  const userName = "Rohit";
+  const balance = "10000";
+  const Users =[]
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<SignUp/>}/>
-          <Route path="/signup" element={<SignIn/>}/>
-          <Route path="/signup" element={<Dashboard/>}/>
-          <Route path="/signup" element={<Send/>}/>
+          <Route path="/signin" element={<SignIn/>}/>
+          <Route path="/dashboard" userName={userName}
+          balance={balance} users={Users}
+          element={<Dashboard/>}/>
+          <Route path="/send" element={<Send/>}/>
   
         </Routes>
       </BrowserRouter>
     </div>
   )
+  
 }
 
 export default App
--- frontend/src/routes/signup.jsx
+++ frontend/src/routes/signup.jsx
@@ -1,27 +1,87 @@
-export const SignUp = ()=>{
- return <>
-   <div className="container">
-        <h2>Sign Up</h2>
-        <form action="signup.php" method="post">
-            <div className="input-group">
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required/>
-            </div>
-            <div className="input-group">
-                <label htmlFor="firstName">First Name:</label>
-                <input type="text" id="firstName" name="firstName" required/>
-            </div>
-            <div className="input-group">
-                <label htmlFor="lastName">Last Name:</label>
-                <input type="text" id="lastName" name="lastName" required/>
-            </div>
-            <div className="input-group">
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" required/>
-            </div>
-            <button type="submit" className="btn">Sign Up</button>
-        </form>
-        <p>Already have an account? <a href="login.php">Login</a></p>
-    </div>
- </>
-}
+export const SignUp = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen bg-[#7f7f7f]">
+        <div className="border bg-[#ffffff] rounded-lg">
+        <div className=" mx-12  ">
+          <h2 className="text-center pt-8 text-4xl font-bold ">Sign Up</h2>
+          <h3 className="text-center text-lg">
+            Enter your informatiopn to create an
+            <br /> account
+          </h3>
+          </div>
+          <div className=" p-3 pt-10 pb-5 px-4">
+            <form
+              action="signup.php"
+              method="post"
+              className="text-lg font-semibold"
+            >
+              <div className="input-group w-full ">
+                <label htmlFor="firstName" className="">First Name</label>
+                <br />
+                <div className="py-2">
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  placeholder="Rohit kumar"
+                  className="border-2 rounded-lg w-full p-2 text-lg font-medium"
+                  required
+                /></div>
+              </div>
+              <div className="input-group">
+                <label htmlFor="lastName">Last Name</label>
+                <br />
+                <div className="py-2">
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName" 
+                  placeholder="Barada"
+                  className="border-2 rounded-lg w-full p-2 text-lg font-medium"
+                  required
+                /></div>
+              </div>
+              <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <br />
+                <div className="py-2">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="rohitkumarbarada@gmail.com"
+                  className="border-2 rounded-lg w-full p-2 text-lg font-medium"
+                  required
+                /></div>
+              </div>
+              <div className="input-group pb-4">
+                <label htmlFor="password">Password</label>
+                <br />
+                <div className="pt-2">
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="******"
+                  className="border-2 rounded-lg w-full  p-2 text-lg font-medium"
+                  required
+                /></div>
+              </div>
+
+              <button
+                type="submit"
+                className="w-full border text-center text-sm bg-black text-white pt-1.5 rounded-lg pb-1.5"
+              >
+                Sign Up
+              </button>
+            </form>
+          </div>
+          <p className="text-center pb-6">
+            Already have an account? <button className="underline">Login</button>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
