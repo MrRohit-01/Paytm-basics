25402f09abd864773cc3ba9d77e94b0d077b5940
Rohit Kumar Barada <rohitbarada.com>
2024-06-23 00:55:46 +0530

    minor issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route,useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SignUp } from "./routes/signup";
 import { LogIn } from "./routes/login";
 import Dashboard from "./routes/dashbord";
