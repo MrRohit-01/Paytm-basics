f71733f0cf3d6651e95d025b7a2bbe06391ad38b
Rohit Kumar Barada <rohitbarada.com>
2024-06-25 17:12:12 +0530

    import issues

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require("cors")
-const routerSchema = require("./routes/index.mjs")
+const routerSchema = require("./routes/index.js")
 const userRouter = require("./routes/user")
-const accountRouter = require("./routes/account.mjs")
+const accountRouter = require("./routes/account.js")
  app.use(express.json())
  app.use(cors())
 app.use("/api/v1",routerSchema);
